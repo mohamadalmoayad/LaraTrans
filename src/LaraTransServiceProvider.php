da90831d1a2f3e789ac93075c4611613ab955ad5
mohamadalmoayad <mohamadalmoayad@gmail.com>
2024-11-01 11:44:39 +0200

    update LaraTransServiceProvider to include configs

--- src/LaraTransServiceProvider.php
+++ src/LaraTransServiceProvider.php
@@ -13,6 +13,17 @@ class LaraTransServiceProvider extends ServiceProvider
             __DIR__.'/Database/Migrations/create_LaraTrans_translations_table.php' => database_path('migrations/'.date('Y_m_d_His').'_create_LaraTrans_translations_table.php'),
         ], 'migrations');
 
+        // Publish config
+        $this->publishes([
+            __DIR__ . '/Config/laratrans.php' => config_path('laratrans.php'),
+        ], 'config');
+
+        // Merge config
+        $this->mergeConfigFrom(
+            __DIR__ . '/Config/laratrans.php',
+            'laratrans'
+        );
+
         // Register the translation trait
         $this->registerTranslations();
     }
