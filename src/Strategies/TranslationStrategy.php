1ffb381cfc033e3efd48e58cc856b5e050032962
mohamadalmoayad <mohamadalmoayad@gmail.com>
2024-12-07 15:58:47 +0200

    Add TranslationStrategy abstract class for translation handling

---
+++ src/Strategies/TranslationStrategy.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Almoayad\LaraTrans\Strategies;
+
+use Illuminate\Database\Eloquent\Model;
+
+abstract class TranslationStrategy
+{
+    protected Model $model;
+
+    public function __construct(Model $model)
+    {
+        $this->model = $model;
+    }
+
+    abstract public function getTranslation(string $property, string $locale): ?string;
+    abstract public function setTranslation(string $property, string $value, string $locale): void;
+    abstract public function deleteTranslations(): void;
+    abstract public function getTranslationsQuery(string $locale = null);
+    abstract public function createTranslations(array $translations): void;
+    abstract public function getTableName(): string;
+}
